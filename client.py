1f08b7d393103f6dbbdc75a5b403fa52799b5df0
Ahmet Duran <66683435+siriiuss@users.noreply.github.com>
2022-02-11 23:47:28 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import colorama
 from colorama import Fore
 import json
 import logo
+import vpnsettings
 
 logo.logo_text()
 sign = "siriOS-VPN"
@@ -14,35 +15,41 @@ with open("data.json") as jsonFile:
     jsonFile.close()
 
 def setup():
-    if len(jsonRead['lang']) == 0:
-        print(sign + "Please select your language: TR | EN")
-        lang = input(sign + ": ")
+    if len(jsonRead['OS']) == 0:
+        print(sign + "Please select your OS: Linux | Windows | Mac")
+        lang = input(sign + ":\t")
 
-        if lang == "TR" or lang == "tr":
-            print("your language setting has been successfully set to: " + lang)
-
-        elif lang == "EN" or lang == "EN":
+        if lang == "Linux" or lang == "Windows" or lang == "MacOS" or lang == "Lin" or lang == "Win" or lang == "Mac":
             print("Your language setting has been successfully set to: " + lang)
 
-        elif lang != "TR" or "tr" or "EN" or "en":
+        elif lang != "Linux" or "Windows" or "MacOS" or "Win" or "Mac" or "Lin":
             print("Please enter a valid option.")
             return setup()
     else:
         pass
+def ui():
+    if len(jsonRead['OS']) >= 1 and jsonRead['OS'] == "Win":
+        print("1) New VPN connection")
+        print("2) Show VPN profiles")
+        print("3) Go Windows VPN Settings")
+        print("4) Settings")
+        inpWin = int(input(sign +" | Please select:\t"))
 
-    if len(jsonRead['lang']) >= 1 and jsonRead['lang'] == "TR":
-        print("New VPN connection")
-        print("Show VPN profiles")
-        print("Settings")
-        transwer = int(input(sign +" | Please select:  "))
-
-        if transwer == 1 and jsonRead['OS'] == "Win":
+        if inpWin == 1:
             print("Name of connection:")
             name = input(sign + ":  ")
             print("IP/server adress:")
             ip = input(sign + ":  ")
             print("IPSec PSK:")
-            ipsecpsk = input(sign + ":  ")
+            ipsecpsk = input(sign + ":\t")
 
             subprocess.call(rf'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe,Add-VpnConnection -Name {name} -ServerAddress {ip} -L2tpPsk {ipsecpsk} -TunnelType L2tp -EncryptionLevel Required -AuthenticationMethod Chap,MSChapv2 -Force -RememberCredential -PassThru', shell=True)
+        elif inpWin == 3:
+            vpnsettings.vpn_settings()
+
+
+        elif inpWin != 1 or jsonRead['OS'] != "Win" or inpWin != 2 or inpWin != 3 or inpWin != 4:
+            print(sign + "Please enter a valid option.")
+            return ui()
 setup()
+ui()
