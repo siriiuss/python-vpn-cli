86c8b94c2b2cb7dda5b963ca5729371de2947acd
Ahmet Duran <66683435+siriiuss@users.noreply.github.com>
2022-02-12 23:12:02 +0300

    Update client.py

--- client.py
+++ client.py
@@ -13,21 +13,20 @@ with open("data.json") as jsonFile:
     jsonRead = json.load(jsonFile)
     jsonWrite = json.dump
     jsonFile.close()
-    
-with open("data.json", "w") as f:
-    jsonRead["OS"] = lang
-    json.dump(jsonRead, f, indent=4)
-    f.close()
 
 def setup():
     if len(jsonRead['OS']) == 0:
         print(sign + "Please select your OS: Linux | Windows | Mac")
-        lang = input(sign + ":\t")
+        os = input(sign + ":\t")
+        with open("data.json", "w") as f:
+            jsonRead["OS"] = os
+            json.dump(jsonRead, f, indent=4)
+            f.close()
 
-        if lang == "Linux" or lang == "Windows" or lang == "MacOS" or lang == "Lin" or lang == "Win" or lang == "Mac":
-            print("Your language setting has been successfully set to: " + lang)
+        if os == "Linux" or os == "Windows" or os == "MacOS" or os == "Lin" or os == "Win" or os == "Mac":
+            print("Your language setting has been successfully set to: " + os)
 
-        elif lang != "Linux" or "Windows" or "MacOS" or "Win" or "Mac" or "Lin":
+        elif os != "Linux" or "Windows" or "MacOS" or "Win" or "Mac" or "Lin":
             print("Please enter a valid option.")
             return setup()
     else:
@@ -35,8 +34,8 @@ def setup():
 def ui():
     if len(jsonRead['OS']) >= 1 and jsonRead['OS'] == "Win":
         print("1) New VPN connection")
-        print("2) Show VPN profiles")
-        print("3) Go Windows VPN Settings")
+        print("2) Go Windows VPN Settings")
+        print("3) Show VPN profiles")
         print("4) Settings")
         inpWin = int(input(sign +" | Please select:\t"))
 
@@ -48,9 +47,27 @@ def ui():
             print("IPSec PSK:")
             ipsecpsk = input(sign + ":\t")
 
-            subprocess.call(rf'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe,Add-VpnConnection -Name {name} -ServerAddress {ip} -L2tpPsk {ipsecpsk} -TunnelType L2tp -EncryptionLevel Required -AuthenticationMethod Chap,MSChapv2 -Force -RememberCredential -PassThru', shell=True)
-        elif inpWin == 3:
+            subprocess.call(["powershell","Add-VpnConnection -Name 132 -ServerAddress 321 -L2tpPsk 321 -TunnelType L2tp -EncryptionLevel Required -AuthenticationMethod Chap,MSChapv2 -Force -RememberCredential -PassThru"],shell=True)
+        elif inpWin == 2:
             vpnsettings.vpn_settings()
+
+        elif inpWin == 3:
+            subprocess.call(["powershell", "Get-VpnConnection"], shell=True)
+
+        elif inpWin == 4:
+            print(sign + "Select your operating system: [Win | Linux | MacOS")
+            settingOS = input(sign + "\t")
+
+            if settingOS == "Win" or "Windows":
+                print(sign + "Please select your OS: Linux | Windows | Mac")
+
+
+            elif settingOS == "Linux" or "Lin":
+                print(sign + "Please select your OS: Linux | Windows | Mac")
+
+
+            elif settingOS == "Mac" or "MacOS":
+                print(sign + "Please select your OS: Linux | Windows | Mac")
 
 
         elif inpWin != 1 or jsonRead['OS'] != "Win" or inpWin != 2 or inpWin != 3 or inpWin != 4:
